5a765d0d1fb2f803718f8e8f8a73182ae37dd47f
AppleInside <carsa.26@gmail.com>
2018-03-05 18:51:42 +0100

    BUGIFX

--- Matlab_Test/src/MatlabTest.java
+++ Matlab_Test/src/MatlabTest.java
@@ -21,9 +21,6 @@ public class MatlabTest {
 		// Start MATLAB Session
 		MatlabEngine matlab = MatlabEngine.startMatlab();
 		
-		double[] vect = {1, 2, 2};
-		int[] vect2 = {3, 6, 2};
-		double[] angles = {30, 0, 90};	// Degrees of Radiants
 		StringWriter writer = new StringWriter();
 		
 		matlab.eval("cd matlab_scripts");
@@ -32,29 +29,29 @@ public class MatlabTest {
         
         matlab.eval("clearMem;");
 		
-        //Object obj1 = matlab.feval("differenza",vect, vect2);
-        //Object obj1 = matlab.feval("ruotaTerna",vect, angles, "deg");
+        /*	RUOTA TERNA
+        double[] vect = {1, 2, 2};
+		int[] vect2 = {3, 6, 2};
+		double[] angles = {30, 0, 90};	// Degrees of Radiants
+		
+		Object obj = matlab.feval("ruotaTerna",vect, angles, "deg");
+        */
         
-        matlab.eval("global sumVar");
+		
+        
+        /* VARIABLES VISIBILITY */
+		matlab.eval("global sumVar");
         matlab.eval("sumVar = 10;");
+        
+        matlab.feval(0,"sommaNum", 0);
+         
+        
         
         
         // Salvo tutte le variabili del workspace in un file .mat
+        
         //matlab.eval("save('workspaceVars');");
         //matlab.eval("whos -f workspaceVars.mat;", writer, null);
-        
-        // Chiama 10 volte questa funzione 
-        Object obj1 = null;
-        /*
-        for (int i =0; i<10; i++)
-        	// 0 stà per "nessun output previsto"
-        	matlab.feval(0,"evalin('base'),sommaNum)", i);
-        */
-     	matlab.feval(0,"sommaNum", 0);
-         
-        // Verifico il risultato finale
-        obj1 = matlab.getVariable("sumVar");
-        System.out.println("obj1.class : "+obj1.getClass().getSimpleName()+" -> value: "+obj1);
         
         
         // Close MATLAB Session
