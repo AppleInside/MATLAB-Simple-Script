e2833b04e90aba5a049133ce1d4120bbd3e1d828
AppleInside <carsa.26@gmail.com>
2018-03-05 18:46:50 +0100

    bugfix

---
+++ Matlab_Test/matlab_scripts/clearMem.m
@@ -0,0 +1,6 @@
+% clear removes all variables from the current workspace, releasing them from system memory.
+clear all
+% close all deletes all figures whose handles are not hidden
+close all
+% clc clears all input and output from the Command Window display, giving you a ?clean screen.?
+clc
--- Matlab_Test/src/MatlabTest.java
+++ Matlab_Test/src/MatlabTest.java
@@ -4,6 +4,7 @@
  *
  */
 
+import java.io.StringWriter;
 import java.util.concurrent.ExecutionException;
 
 import java.util.concurrent.RejectedExecutionException;
@@ -16,30 +17,48 @@ public class MatlabTest {
 	public static void main(String[] args) throws IllegalArgumentException, IllegalStateException, InterruptedException, RejectedExecutionException, ExecutionException {
 		
 		System.out.println("Inizializzo MATLAB Engine...");
+		
+		// Start MATLAB Session
 		MatlabEngine matlab = MatlabEngine.startMatlab();
+		
 		double[] vect = {1, 2, 2};
 		int[] vect2 = {3, 6, 2};
 		double[] angles = {30, 0, 90};	// Degrees of Radiants
+		StringWriter writer = new StringWriter();
 		
 		matlab.eval("cd matlab_scripts");
-        matlab.eval("cd");
+        //matlab.eval("cd");
+        //matlab.eval("ls");
         
-        
+        matlab.eval("clearMem;");
+		
         //Object obj1 = matlab.feval("differenza",vect, vect2);
         //Object obj1 = matlab.feval("ruotaTerna",vect, angles, "deg");
         
+        matlab.eval("global sumVar");
         matlab.eval("sumVar = 10;");
+        
+        
+        // Salvo tutte le variabili del workspace in un file .mat
+        //matlab.eval("save('workspaceVars');");
+        //matlab.eval("whos -f workspaceVars.mat;", writer, null);
+        
         // Chiama 10 volte questa funzione 
         Object obj1 = null;
-        //for (int i =0; i<10; i++)
-        	//obj1 = matlab.feval("somma", i);
-        
-        matlab.feval("somma", 10);
-        
-        
+        /*
+        for (int i =0; i<10; i++)
+        	// 0 stà per "nessun output previsto"
+        	matlab.feval(0,"evalin('base'),sommaNum)", i);
+        */
+     	matlab.feval(0,"sommaNum", 0);
+         
         // Verifico il risultato finale
         obj1 = matlab.getVariable("sumVar");
-        System.out.println("obj1.class : "+obj1.getClass().getSimpleName());
+        System.out.println("obj1.class : "+obj1.getClass().getSimpleName()+" -> value: "+obj1);
+        
+        
+        // Close MATLAB Session
+        matlab.close();
         
 	}
 
